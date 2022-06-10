c23c8c12bdffc82c7d74bc2ff48ea51311b6fefc
amorim.pedro@engenharia.ufjf.br <amorim.pedro@engenharia.ufjf.br>
2022-06-10 09:21:53 -0300

    Fix bugs in routers auth and feature middlewares auth

--- pages/routers/auth1/middlewares.jsx
+++ pages/routers/auth1/middlewares.jsx
@@ -1,9 +1,14 @@
 import db from '../../db.jsx';
-
-export const validateCredentials = async (req, res, next) => {
+import { SignInSchema, UserSchema } from '../validation/schemas.js';
+import bcrypt from 'bcrypt';
+export const validateToken= async (req, res, next) => {
     const { email, password } = req.body;
     try {
-
+        const result = await db.query(`SELECT * FROM users WHERE email = $1`, [email]);
+        if (result.rows.length === 0) return res.status(409).send({ error: "Email not registered" });
+        const user = result.rows[0];
+        if (!bcrypt.compareSync(password, user.password)) return res.status(401).send({ error: "Wrong Password" });
+        res.locals.user = user;
         next();
     } catch (err) {
         res.status(500).send({ error: err });
@@ -13,6 +18,9 @@ export const validateCredentials = async (req, res, next) => {
 export const validateEmailAvailable = async (req, res, next) => {
     const { email } = req.body;
     try {
+        const query = `SELECT * FROM users WHERE email = '${email}'`;
+        const result = await db.query(query);
+        if (result.rows.length > 0) return res.status(409).send({ error: "Email already registered" });
         next();
     } catch (err) {
         res.status(500).send({ error: err });
@@ -20,11 +28,14 @@ export const validateEmailAvailable = async (req, res, next) => {
 };
 
 export const validateSignInBody = (req, res, next) => {
+    const { error } = SignInSchema.validate(req.body);
+    if (error) return res.status(422).send({ error: error.details[0].message });
     
     next();
 };
 
 export const validateUserBody = (req, res, next) => {
-    
+    const { error } = UserSchema.validate(req.body);
+    if (error) return res.status(422).send({ error: error.details[0].message });
     next();
 };
